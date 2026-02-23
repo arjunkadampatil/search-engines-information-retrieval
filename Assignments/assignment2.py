b9c80d9216870c72dc5caacee3b0715de82cc6c4
Arjun Kadam <arjunkadampatil693@gmail.com>
2026-02-23 22:39:15 +0530

    Refactor webpage body fetching and word frequency functions

--- Assignments/assignment2.py
+++ Assignments/assignment2.py
@@ -3,64 +3,65 @@
 # ------------------------------------------------------
 
 
+# required libraries
 import requests
 import re
 from bs4 import BeautifulSoup
 
-# fetch webpage body
-def get_body(url):
-    r=requests.get(url)
-    soup=BeautifulSoup(r.text,'html.parser')
-    if soup.body:
-        return soup.body.get_text(separator=' ',strip=True)
+# fetching the body of webpage
+def getBody(weburls):
+    r=requests.get(weburls)
+    s=BeautifulSoup(r.text,'html.parser')
+    if s.body:
+        return s.body.get_text(separator='',strip=True)
     return ""
 
-# word frequency
-def word_freq(text):
-    words=re.findall(r'[A-Za-z0-9]+',text.lower())
-    freq={}
-    for w in words:
-        if w in freq:
-            freq[w]+=1
-        else:
-            freq[w]=1
-    return freq
+# rolling hash (polynomial)
+a=53
+b=2**64
+def rollingHash(w):
+    c=0
+    for i,j in enumerate(w):
+        c+=ord(j)*(a**i)
+    return c%b
 
-# polynomial rolling hash
-P=53
-M=2**64
-def rolling_hash(word):
-    h=0
-    for i,ch in enumerate(word):
-        h+=ord(ch)*(P**i)
-    return h%M
-
-# simhash
-def simhash(freq):
-    vec=[0]*64
-    for word,count in freq.items():
-        h=rolling_hash(word)
-        for i in range(64):
-            bit=(h>>i)&1
-            if bit==1:
-                vec[i]+=count
+# simhash function
+def simhashing(f):
+    v=[0]*64
+    for i,j in f.items():
+        h=rollingHash(i)
+        for k in range(64):
+            bt=(h>>k)&1
+            if bt==1:
+                v[k]+=j
             else:
-                vec[i]-=count
-    final=0
-    for i in range(64):
-        if vec[i]>0:
-            final|=(1<<i)
-    return final
+                v[k]-=j
+    f=0
+    for l in range(64):
+        if v[l]>0:
+            f|=(1<<l)
+    return f
 
-# compare two urls
-url1=input("1st URL: ")
-url2=input("2nd URL: ")
-text1=get_body(url1)
-text2=get_body(url2)
-freq1=word_freq(text1)
-freq2=word_freq(text2)
-s1=simhash(freq1)
-s2=simhash(freq2)
-xor=s1^s2
-same_bits=64-bin(xor).count('1')
-print("Common bits = ",same_bits)
+# frequency of the words
+def wordFreq(txt):
+    word=re.findall(r'[A-Za-z0-9]+')
+    f={}
+    for i in word:
+        if i in f:
+            f[i]+=1
+        else:
+            f[i]=1
+    return f
+
+# comparision of the two urls
+url1=input("1st url")
+url2=input("2nd url")
+txt1=getBody(url1)
+txt2=getBody(url2)
+f1=wordFreq(txt1)
+f2=wordFreq(txt2)
+s1=simhashing(f1)
+s2=simhashing(f2)
+x=s1^s2
+sameBits=64-bin(x).count('1')
+print("Common bits=",sameBits)
